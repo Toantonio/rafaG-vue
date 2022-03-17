55fc213a665b12eb38cfe309947109987b1b89c8
antonio <toantonio48@gmail.com>
2022-03-17 20:04:32 -0300

    inicio

---
+++ src/components/Demonstracao.vue
@@ -0,0 +1,33 @@
+<template>
+<section >
+	<div id="layout" class="container p-0 " >	
+		<img src="img/produtos/p2-3.jpg" alt="" class="img-1 img-fluid" >
+
+	</div>
+</section>
+<section id="demosnstracao" class=" container my-2 bg-blue-white">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="img/produtos/bake-1003685_960_720.jpg" class=" carrcel car-3 d-block w-100" alt="pro-3">
+            </div>
+            <div class="carousel-item">
+            <img src="img/produtos/produto03.jpg" class="carrcel car-1 d-block w-100" alt="pro-2">
+            </div>
+            <div class="carousel-item">
+            <img src="img/produtos/produto01.jpg" class="carrcel car-2 d-block w-100" alt="pro-1">
+            </div>
+        </div>
+        </div>
+</section>
+</template>
+
+<script>
+export default {
+    name:"DemonsTracao",
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,72 @@
+<template>
+<header  class=" container  ">
+		<div class="row">		
+			<div class="logo col-md-3 ">
+				<img asrc="./assets\img\RafaGourmetlogo.png" class="imglogo px-3 img-fluid">
+				<img src="src\assets\img\RafaGourmetlogo.png"  class="imglogo px-3 img-fluid">
+			</div>
+			<div class="col-md-3"></div>
+			
+		</div>	
+	</header>
+  <nav class="navbar navbar-expand-lg navbar-light bg-orang container sticky-top ">
+        <div class="container-fluid">
+          <a class="navbar-brand orang1" href="#">RafaGourmet</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">inicio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="pages/logon/logon.html" target="blanck">Entrar</a>
+              </li>
+			<li class="nav-item">
+                <a class="nav-link" href="pages/carrinho/carrinho.html" target="blanck">Carrinho</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Quem somos</a></li>                
+                </ul>
+              </li>
+          
+            </ul>
+			
+          </div>
+		<div class="container-fluid col-md-6 p-0">
+			<form class="d-flex">
+			<input class="form-control me-2" type="search" placeholder="buscar" aria-label="Search">
+			<button class="btn btn-outline-success" type="submit">Buscar</button>
+			</form>
+		</div>
+        </div>
+      </nav>
+</template>
+
+<script>
+export default {
+    name:"NavBar",
+}
+</script>
+
+<style>
+header{ 
+    background-color: #2e5566;
+    min-height: 80px;	
+}
+.logo{
+    text-align: center;
+    min-height:80px ;
+}
+.imglogo{
+    height: 80px; 
+}
+.img-1{
+    width: 100%;
+}
+</style>
