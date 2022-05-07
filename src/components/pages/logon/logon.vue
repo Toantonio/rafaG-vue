199c89e7bab89d589ef27e6b0ddc1fffaa1348bf
Fabiano Moreira <fabiano.moreira@atak.com.br>
2022-05-06 21:54:43 -0300

    ok

--- src/components/pages/logon/logon.vue
+++ src/components/pages/logon/logon.vue
@@ -1,70 +1,101 @@
 <template>
-    <section id="logon" class=" container ">
-        <div class="row my-5">	
-            <div class="col-md-4"></div>
-                <div class="col-md-4 col-12 bor-1 rounded">
-                    <form action="" method="post">
-                        <h2>Fazer Login</h2>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Email </label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                            <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
-                            <a href="ms-auto">Esqueceu</a> ou <a href="">Cadastrese</a>
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Senha</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1">
-                            <a href="">Esqueceu</a>
-                        </div>
-                        <div class="mb-3 form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Mantenha-me conectado</label>
-                        </div>
-                        <div class="text-center">
-                            <button type="button " class="btn px-5  orang bg-blue  cal-12" @click="loganUsuario()">LOGIN</button>
-                            <!-- <button type="button" class="col-12 rounded-3 bg-orang pb-1 bor_1 mt-3"> -->
-							<!-- <a href="/ProdutoVenda" target="blanck" class="white" >COMPRAR</a> -->
-						<router-link to="/logonCadas" class="btn col-12 rounded-3 bg-orang pb-1 bor_1 mt-3 text-danger"><b>cadastre-se</b> </router-link>
-							<!-- </button> -->
-                        </div>
-                    </form>
-                </div>
-            <div class="col-nd-4"></div>
+  <section id="logon" class="container">
+    <div class="row my-5">
+      <div class="col-md-4"></div>
+      <div class="col-md-4 col-12 bor-1 rounded">
+        <!-- <form action="" method="post"> -->
+        <h2>Fazer Login</h2>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email </label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            v-model="dadosUsuario.usuario"
+          />
+          <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+          <!-- <a href="ms-auto">Esqueceu</a> ou <a href="">Cadastrese</a> -->
         </div>
-    </section>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Senha</label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+            v-model="dadosUsuario.senha"
+          />
+          <!-- <a href="">Esqueceu</a> -->
+        </div>
+        <!-- <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+          <label class="form-check-label" for="exampleCheck1"
+            >Mantenha-me conectado</label
+          >
+        </div> -->
+        <div class="text-center">
+          <button
+            type="button"
+            class="btn px-5 orang bg-blue col-12"
+            @click="loganUsuario()"
+          >
+            Entrar
+          </button>
+
+          <router-link
+            type="button"
+            class="btn px-5 orang bg-blue col-12"
+            to="/"
+          >
+            Sair
+          </router-link>
+          <!-- <button type="button" class="col-12 rounded-3 bg-orang pb-1 bor_1 mt-3"> -->
+          <!-- <a href="/ProdutoVenda" target="blanck" class="white" >COMPRAR</a> -->
+          <router-link
+            to="/logonCadas"
+            class="btn col-12 rounded-3 bg-orang pb-1 bor_1 mt-3 text-danger"
+            ><b>cadastre-se</b>
+          </router-link>
+          <!-- </button> -->
+        </div>
+        <!-- </form> -->
+      </div>
+      <div class="col-nd-4"></div>
+    </div>
+  </section>
 </template>
 
 <script>
-
-
 import UsuarioService from "@/services/usuario";
+import router from "@/router";
 
 export default {
-     name:'logonUser',
-  components:{
-   
-  },
+  name: "logonUser",
+  components: {},
   data() {
-    // return {
-    //   dadosUsuario: {
-    //     usuario: "",
-    //     senha: "",
-    //   },
-    // };
+    return {
+      dadosUsuario: {
+        usuario: "",
+        senha: "",
+      },
+    };
   },
   mounted() {
-    this.listarUsuarios();
+    //this.listarUsuarios();
   },
   methods: {
-    listarUsuarios() {
-      UsuarioService.list().then((res) => {
-        console.log(res);
-      });
-    },
+    // listarUsuarios() {
+    //   UsuarioService.list().then((res) => {
+    //     console.log(res);
+    //   });
+    // },
     loganUsuario() {
       UsuarioService.login(this.dadosUsuario)
         .then((res) => {
-          console.log(res);
+          alert("Usuario encontrado!");
+          localStorage.setItem("token", res.data.result.token);
+          localStorage.setItem("user", JSON.stringify(res.data.result));
+          router.push("/");
         })
         .catch((err) => {
           console.error(err);
@@ -75,5 +106,4 @@ export default {
 </script>
 
 <style>
-
 </style>
--- src/services/config.js
+++ src/services/config.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
-export const confirure ={
-    localAPI: location.origin +"/api/"
+
+export const confirure = {
+    localAPI: location.origin + "/api/"
 };
+
 export const http = axios.create({
-    // baseURL: "http://localhost/api/",
+    baseURL: "http://localhost/api/",
+    //baseURL: confirure.localAPI,
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json;charset=utf-8',
